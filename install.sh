50d9d9253264f4716e398dbdb20674672d678cb4
Никита Елисеев <57886942+angrydish@users.noreply.github.com>
2023-05-03 22:49:40 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install curl -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo gpasswd -a $USER docker
+echo "PROPISHI exit POZHALUISTA"
+newgrp docker
+docker --version
+docker compose version
