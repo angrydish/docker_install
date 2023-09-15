501aeed28281dc4d73b688806ffa01d4b1ba50e7
angrydish <yeliseev2003@gmail.com>
2023-09-15 09:37:12 +0000

    update and optimization of installing script

--- install.sh
+++ install.sh
@@ -4,11 +4,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
-sudo apt install docker-ce -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo gpasswd -a $USER docker
-echo "PROPISHI exit POZHALUISTA"
-newgrp docker
+sudo apt install docker.io docker-ce -y
 docker --version
 docker compose version
+echo "if you see versions of docker and docker compose, then they were successfully installed"
