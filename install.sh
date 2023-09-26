c7c56f924a8288cd2d8365871978aebca35377ae
Никита Елисеев <57886942+angrydish@users.noreply.github.com>
2023-09-26 21:11:07 +0700

    Update install.sh
    fix strange bug

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo apt update -y
+sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt update
-sudo apt install docker.io docker-ce -y
+sudo apt install docker.io -y
+sudo apt install docker-ce -y
 docker --version
 docker compose version
 echo "if you see versions of docker and docker compose, then they were successfully installed"
